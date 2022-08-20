2c3bdbb0a920b0aab9f17c24c7faa6a447e9e08d
shirsak123 <shirsak.shrestha@gmail.com>
2022-08-20 15:46:28 +0545

    authenticating login and resgister

---
+++ app/http/controllers/authController.js
@@ -0,0 +1,104 @@
+const User = require('../../models/user')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+var mongoose = require("mongoose") 
+
+function authController() {
+    const _getRedirectUrl = (req) => {
+        return req.user.role === 'admin' ? '/admin/orders' : '/customer/orders'
+    }
+    const postLogin= async function (req, res, next) {
+        const { email, password } = req.body 
+        // Validate request
+        if (!email || !password) {
+          req.flash("error", "All fields are required") 
+          return res.redirect("/login") 
+        }
+        passport.authenticate("local", (err, user, info) => {
+          if (err) {
+            req.flash("error", info.message) 
+            return next(err) 
+          }
+          if (!user) {
+            req.flash("error", info.message) 
+            return res.redirect("/login") 
+          }
+          req.logIn(user, (err) => {
+            if (err) {
+              req.flash("error", info.message) 
+              return next(err) 
+            }
+            req.flash("success", "Logged in successfully");
+            return res.redirect('/')
+          }) 
+        })(req, res, next) 
+      }
+    return {
+        login(req, res) {
+            res.render('auth/login')
+        },
+        postLogin,
+        async register(req, res) {
+            res.render('auth/register')
+        },
+        async postRegister(req, res, next) {
+         const { name, email, password }   = req.body
+         // Validate request 
+         if(!name || !email || !password) {
+             req.flash('error', 'All fields are required')
+             req.flash('name', name)
+             req.flash('email', email)
+            return res.redirect('/register')
+         }
+
+         // Check if email exists 
+         User.exists({ email: email }, (err, result) => {
+             if(result) {
+                req.flash('error', 'Email already taken')
+                req.flash('name', name)
+                req.flash('email', email) 
+                return res.redirect('/register')
+             }
+         })
+
+         // Hash password 
+         const hashedPassword = await bcrypt.hash(password, 10)
+         // Create a user 
+         const user = new User({
+             name,
+             email,
+             password: hashedPassword
+         })
+
+         user.save().then(async(user) => {
+            return await postLogin(req, res, next);
+         }).catch(err => {
+            req.flash('error', 'Something went wrong')
+                return res.redirect('/register')
+         })
+        },
+        logout(req, res) {
+            req.logout(function(err) {
+                if (err) { return (err); }
+                res.redirect('/');
+              });
+        //   req.flash("success", "Logged out successfully")
+          return res.redirect('/login')  
+        },
+      
+    }
+}
+
+module.exports = authController
+
+// app.post('/logout', function(req, res, next) {
+//     req.logout(function(err) {
+//       if (err) { return next(err); }
+//       res.redirect('/');
+//     });
+//   });
+
+
+//  googleLogin(req, res, next) {
+//     return res.redirect(_getRedirectUrl(req))
+// },
